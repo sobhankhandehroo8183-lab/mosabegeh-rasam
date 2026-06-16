e6280cb611cd2d9180aa42f0a26d9693bf9f83fd
sobhan <sobhankhandehroo8181@gmail.com>
2026-06-16 08:47:30 +0330

    Deploy static site - final working version

---
+++ next.config.js
@@ -0,0 +1,9 @@
+const nextConfig = { 
+  output: 'export', 
+  images: { 
+    unoptimized: true, 
+  }, 
+  basePath: '/mosabegeh-rasam', 
+  assetPrefix: '/mosabegeh-rasam', 
+}; 
+module.exports = nextConfig; 
